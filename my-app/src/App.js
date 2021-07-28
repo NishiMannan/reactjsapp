333a599f9cb649e9b339a96f6ecb6bf86568fe91
Nishi Kanta <mannan.nishi@gmail.com>
2021-07-28 14:15:15 -0400

    updated app ,API,useeffect,useState hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 
 
 
-import react from 'react';
+import react, {useState, useEffect} from 'react';
 
 
 import ReactDOM from 'react-dom';
@@ -9,28 +9,25 @@ import logo from './logo.svg';
 import ContactCard from './ContactCard';
 import './App.css';
 
-<html>
-
-<p id ="root"></p>
-
-</html>
-
 
 
 const App = () => {
+    const [results, setcontacts] = useState([]);
+useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=5")
+    .then(response=>response.json())
+    .then(data=>{console.log(data) 
+        setcontacts(data.results)});}, [])
  
-    const contacts = [{avatarurl:"https://via.placeholder.com/150", name:"Rick", email:"rick@gmail.com", age:27},
-     {avatarurl:"https://via.placeholder.com/150", name:"Nanvy", email:"nancyk@gmail.com", age:69}, 
-     {avatarurl:"https://via.placeholder.com/150", name:"leo", email:"leo@gmail.com", age:35}];
+  
+    
 return(
 <div>
-   {contacts.map((contact, index) => { 
-return(<ContactCard avatarurl={contact.avatarurl} name={contact.name} email={contact.email} age={contact.age} />
+   {results.map((contact, index) => { 
+return(<ContactCard avatarurl={contact.picture.large} name={contact.name.first} email={contact.email} age={contact.dob.age} />
    )})}
 
 </div>);}
-/*<ContactCard avatarurl="https://via.placeholder.com/150" name="Nanvy" email="nancyk@gmail.com" age={69} />
-<ContactCard avatarurl="https://via.placeholder.com/150" name="leo" email="leo@gmail.com" age={35} />*/
 
 
   
