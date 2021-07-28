16604efce2f250d9b8046b223b7774d4eeea74bc
Nishi Kanta <mannan.nishi@gmail.com>
2021-07-28 02:14:30 -0400

    rendering data using an array and map()

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,16 +19,23 @@ import './App.css';
 
 const App = () => {
  
+    const contacts = [{avatarurl:"https://via.placeholder.com/150", name:"Rick", email:"rick@gmail.com", age:27},
+     {avatarurl:"https://via.placeholder.com/150", name:"Nanvy", email:"nancyk@gmail.com", age:69}, 
+     {avatarurl:"https://via.placeholder.com/150", name:"leo", email:"leo@gmail.com", age:35}];
 return(
 <div>
-<ContactCard avatarurl="https://via.placeholder.com/150" name="Rick" email="rick@gmail.com" age={27} />
-<ContactCard avatarurl="https://via.placeholder.com/150" name="Nanvy" email="nancyk@gmail.com" age={69}/>
-<ContactCard avatarurl="https://via.placeholder.com/150" name="leo" email="leo@gmail.com" age={35}/>
-</div>
-);
+   {contacts.map((contact, index) => { 
+return(<ContactCard avatarurl={contact.avatarurl} name={contact.name} email={contact.email} age={contact.age} />
+   )})}
+
+</div>);}
+/*<ContactCard avatarurl="https://via.placeholder.com/150" name="Nanvy" email="nancyk@gmail.com" age={69} />
+<ContactCard avatarurl="https://via.placeholder.com/150" name="leo" email="leo@gmail.com" age={35} />*/
+
+
   
 
-}
+
 export default App;
 
 
