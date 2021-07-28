16d02396c31c4047a999b233331798c5e0a6b351
Nishi Kanta <mannan.nishi@gmail.com>
2021-07-27 20:12:59 -0400

    react components and props to render data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,38 @@
+
+
+
 import react from 'react';
+
+
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-
+import ContactCard from './ContactCard';
 import './App.css';
+
+<html>
+
+<p id ="root"></p>
+
+</html>
 
 
 
 const App = () => {
  
 return(
-
-<div className="contact-card">
-<img src="https://via.placeholder.com/150" alt="profile"></img>
-
-
-<div className="">
-<p>Name:Nishi</p>
-
-<p>Email Address:mannan.nishi@gmail.com</p>
-<p>Age:30</p>
-
+<div>
+<ContactCard avatarurl="https://via.placeholder.com/150" name="Rick" email="rick@gmail.com" age={27} />
+<ContactCard avatarurl="https://via.placeholder.com/150" name="Nanvy" email="nancyk@gmail.com" age={69}/>
+<ContactCard avatarurl="https://via.placeholder.com/150" name="leo" email="leo@gmail.com" age={35}/>
 </div>
-
-</div>
-
-
 );
   
+
 }
-
-
-
-  
-
-
-
 export default App;
+
+
+
+
+
+
